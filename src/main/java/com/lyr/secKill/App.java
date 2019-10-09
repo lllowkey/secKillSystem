cf356da6be1c4ede8b5c4b443c9fd8c6e60a8bbb
lllowkey <351957049@qq.com>
2019-10-09 18:03:50 +0800

    创建了用户，用户密码表,使用mybatisGenerator

--- src/main/java/com/lyr/secKill/App.java
+++ src/main/java/com/lyr/secKill/App.java
@@ -1,13 +1,41 @@
 package com.lyr.secKill;
 
+import com.lyr.secKill.dao.UserDOMapper;
+import com.lyr.secKill.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
 /**
  * Hello world!
  *
  */
+
+@SpringBootApplication(scanBasePackages = {"com.lyr.secKill"})
+@RestController
+@MapperScan("com.lyr.secKill.dao")
 public class App 
 {
+    @Autowired
+    private
+    UserDOMapper userDOMapper;
+    @RequestMapping("/")
+    public String home(){
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO == null){
+            return "用户对象不存在";
+        }else{
+            return userDO.getName();
+        }
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SpringApplication.run(App.class,args);
     }
 }
