9d8db4946cc90cb9d195fe9b12befbeec0715d75
lllowkey <351957049@qq.com>
2019-10-09 10:25:19 +0800

    project init

---
+++ src/main/java/com/lyr/secKill/App.java
@@ -0,0 +1,13 @@
+package com.lyr.secKill;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
